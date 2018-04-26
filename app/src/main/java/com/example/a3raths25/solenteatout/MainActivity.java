bb7413b8b085ea50c1fc49926f5a0f46341f6f83
sanah13 <Freaks123>
2018-04-26 12:31:19 +0100

    asfdsf

--- app/src/main/java/com/example/a3raths25/solenteatout/MainActivity.java
+++ app/src/main/java/com/example/a3raths25/solenteatout/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.a3raths25.solenteatout;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +17,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
@@ -22,17 +26,17 @@ import org.osmdroid.views.overlay.ItemizedIconOverlay;
 import org.osmdroid.views.overlay.OverlayItem;
 import org.osmdroid.views.overlay.Polyline;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
-import static android.R.attr.name;
-import static android.R.attr.rating;
 import static com.example.a3raths25.solenteatout.R.id.et1;
 import static com.example.a3raths25.solenteatout.R.id.et2;
 import static com.example.a3raths25.solenteatout.R.id.et3;
 import static com.example.a3raths25.solenteatout.R.id.et4;
 
-public class MainActivity extends AppCompatActivity implements  LocationListener {
+public class MainActivity extends AppCompatActivity implements LocationListener {
     MapView mv;
     double lat = 51.05;
     double lon = -1.40435;
@@ -54,24 +58,38 @@ public class MainActivity extends AppCompatActivity implements  LocationListener
         LocationManager mgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         try {
             mgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        } catch (SecurityException e) {
+            e.printStackTrace();
         }
-        catch(SecurityException e) {
-
-        }
-
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
         if (requestCode == 0) {
             if (resultCode == RESULT_OK) {
                 Bundle extras = intent.getExtras();
-
                 String name = extras.getString("name");
-                String address= extras.getString("address");
+                String address = extras.getString("address");
+                String cuisine = extras.getString("cuisine");
+                String rating = extras.getString("rating");
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
-                OverlayItem newResturant = new OverlayItem("name", "address", new GeoPoint(lat, lon));
+                OverlayItem newResturant = new OverlayItem(name, address, new GeoPoint(lat, lon));
                 items.addItem(newResturant);
+                mv.invalidate(); // redraw the map so that the marker is shown immediately
+
                 mv.getOverlays().add(items);
+                try {
+                    PrintWriter pw = new PrintWriter(new FileWriter(Environment.getExternalStorageDirectory().getAbsolutePath() + "/NewResturants.csv"), true);
+                    pw.write(name + ",");
+                    pw.write(address+ ",");
+                    pw.write(cuisine+ ",");
+                    pw.write(rating);
+                    pw.close();
+                } catch (IOException e) {
+                    new AlertDialog.Builder(this).setPositiveButton("OK", null).
+                            setMessage("ERROR: " + e).show();
+                }
 
             }
         }
@@ -82,30 +100,21 @@ public class MainActivity extends AppCompatActivity implements  LocationListener
                         newLoc.getLatitude() + " " +
                         newLoc.getLongitude(), Toast.LENGTH_LONG).show();
         mv.getController().setCenter(new GeoPoint(newLoc.getLatitude(), newLoc.getLongitude()));
-
         lat = newLoc.getLatitude();
         lon = newLoc.getLongitude();
     }
 
     @Override
     public void onStatusChanged(String s, int i, Bundle bundle) {
-
     }
-
     @Override
     public void onProviderEnabled(String s) {
-
     }
-
     @Override
     public void onProviderDisabled(String s) {
-
     }
-
     public void onClick(View view) {
     }
-
-
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
@@ -118,12 +127,22 @@ public class MainActivity extends AppCompatActivity implements  LocationListener
             startActivityForResult(intent, 0);
             return true;
         }
+        if (item.getItemId() == R.id.SaveAllAddedResturantsToFile) {
+            OverlayItem
+
+            //newResturant.getTitle() - gives the title
+            // newResturant.getSnippet() - gives the description
+            //items.getItem(index) - gives the item at position 'index' within the overlay
+            // items.size() - number of OverlayItems on the overlay.
+
+
+            return true;
+        }
         if (item.getItemId() == R.id.preferences) {
             Intent intent = new Intent(this, PrefsActivity.class);
             startActivityForResult(intent, 1);
             return true;
         }
-
         if (item.getItemId() == R.id.LoadFromFile) {
             Intent intent = new Intent(this, LoadFromFileActivity.class);
             startActivityForResult(intent, 2);
--- app/src/main/java/com/example/a3raths25/solenteatout/SaveToFileActivity.java
+++ app/src/main/java/com/example/a3raths25/solenteatout/SaveToFileActivity.java
@@ -12,8 +12,10 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.widget.EditText;
 
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
 import static com.example.a3raths25.solenteatout.R.styleable.MenuItem;
@@ -21,47 +23,17 @@ import static com.example.a3raths25.solenteatout.R.styleable.MenuItem;
 /**
  * Created by 3RATHS25 on 25/04/2018.
  */
-
 public class SaveToFileActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-       MenuInflater inflater = getMenuInflater();
+        MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
     }
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        if (requestCode == 0) {
-            if (resultCode == RESULT_OK) {
-                Bundle extras = intent.getExtras();
-
-                String name = extras.getString("name");
-                String address = extras.getString("address");
-                String cuisine = extras.getString("cuisine");
-                String rating = extras.getString("rating");
-
-                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                prefs.getString("NewResturant");
-                {
-                    try {
-                        PrintWriter pw = new PrintWriter(new FileWriter(Environment.getExternalStorageDirectory().getAbsolutePath() + "/NewResturants.txt"));
-                        pw.write(name);
-                        pw.write(address);
-                        pw.write(cuisine);
-                        pw.write(rating);
-                        pw.close();
-
-                    } catch (IOException e) {
-                        new AlertDialog.Builder(this).setPositiveButton("OK", null).
-                                setMessage("ERROR: " + e).show();
-                    }
-                    return true;
-                }
-            }
-        }
-    }
-
-    @Override
     public void onClick(View view) {
-
     }
 }
+
+
+
+
